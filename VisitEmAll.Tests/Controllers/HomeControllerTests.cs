5bb4402ee43bfe4114a602866f33f59017f691b8
Charlotte Gale <CharlotteMGale@outlook.com>
2026-03-02 12:20:34 +0000

    begins HomeController tests

---
+++ VisitEmAll.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+
+namespace VisitEmAll.Controllers;
+
+public class HomeControllerTests : NUnitTestBase
+{
+    private HomeController _controller;
+
+    [SetUp]
+    public void LocalSetUp()
+    {
+        _controller = new HomeController();
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Session = new MockHttpSession();
+
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext
+        };
+    }
+
+    [TearDown]
+    public void LocalTearDown()
+    {
+        _controller?.Dispose();
+    }
+
+    [Test]
+    public void Index_FirstTimeLanding_ShowsLoginForm()
+    {
+        _controller.HttpContext.Session.Clear();
+
+        var result = _controller.Index() as RedirectToActionResult;
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.ActionName, Is.EqualTo("Login"));
+        Assert.That(result.ControllerName, Is.EqualTo("Auth"));
+    }
+}
